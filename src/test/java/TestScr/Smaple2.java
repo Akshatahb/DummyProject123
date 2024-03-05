f7cfaeff08e26ea3090ce989b025430c6849c705
DivyaDinesh04 <divyaragavan4@gmail.com>
2024-03-05 17:31:41 +0530

    changed b value to c

--- src/test/java/TestScr/Smaple2.java
+++ src/test/java/TestScr/Smaple2.java
@@ -4,9 +4,9 @@ public class Smaple2 {
 
 	public static void main(String[] args) {
 		int a=10;
-		int b=20;
+		int c=20;
 		
-		System.out.println("Sum = "+a+b);
+		System.out.println("Sum = "+a+c);
 
 	}
 
